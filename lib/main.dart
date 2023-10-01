1539481a67fd2d52f82f6ae877d4c85cf48cdf99
Santos1012 <santawyomar@gmail.com>
2023-10-01 09:35:06 +0300

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+// import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tharwat_notes_app/constants.dart';
+import 'package:tharwat_notes_app/models/note_model.dart';
 import 'package:tharwat_notes_app/views/notes_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  var notesListBox = await Hive.openBox<List<NoteModel>>(kNotesListBox);
+
+
   runApp(const NotesApp());
 }
 class NotesApp extends StatelessWidget {
