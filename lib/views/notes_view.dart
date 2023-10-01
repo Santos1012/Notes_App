a46da02ebf52cfe2ec9f97e9b94f09a22dfee17f
Santos1012 <santawyomar@gmail.com>
2023-10-01 03:22:03 +0300

    add border radius to model sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -11,6 +11,13 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+            shape: const RoundedRectangleBorder(
+              
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
+              ),
+            ),
             context: context,
             builder: (context) => const AddNoteBottomSheet(),
           );
