72d4be1f1a67f0b3d00fd314decc13fb03104372
Santos1012 <santawyomar@gmail.com>
2023-10-01 11:37:49 +0300

    register Adapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:tharwat_notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  @override
+  addNote(NoteModel note) {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tharwat_notes_app/models/note_model.dart';
 import 'package:tharwat_notes_app/views/notes_view.dart';
 
 import 'constants.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
-  var notesBox = await Hive.openBox(kNotesListBox);
-
-
+  await Hive.openBox(kNotesListBox);
+  Hive.registerAdapter(NoteModelAdapter());
+  
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
@@ -18,7 +20,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,fontFamily: "Poppins"),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
       home: const NotesView(),
     );
   }
