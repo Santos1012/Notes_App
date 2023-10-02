b828b57dcd015d0e5084898b6cbf1e9eb58c8087
Santos1012 <santawyomar@gmail.com>
2023-10-02 22:06:49 +0300

    create fetchAllNotes and use Blocprovider

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tharwat_notes_app/constants.dart';
 import 'package:tharwat_notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesState> {
-  NotesCubitCubit() : super(NotesInitialState());
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitialState());
+  void fetchAllNotes() {
+    emit(NotesLoadingState());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      //notesBox.values.toList() returne to me List<NoteModel> variable
+      emit(NotesSuccessState(notes: notesBox.values.toList()));
+    } on HiveError catch (e) {
+      emit(NotesFailireState(errMsg: e.toString()));
+    } catch (e) {
+      emit(NotesFailireState(errMsg: e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tharwat_notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:tharwat_notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:tharwat_notes_app/views/widgets/notes_view_body_widget.dart';
 
@@ -7,24 +9,27 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(16),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
               ),
-            ),
-            context: context,
-            builder: (context) => const AddNoteBottomSheet(),
-          );
-        },
-        child: const Icon(Icons.add),
+              context: context,
+              builder: (context) => const AddNoteBottomSheet(),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
-      body: const NotesViewBody(),
     );
   }
 }
